32810d3472023da5149a1767bbba52f9edee104a
Marcelo Fabiano <marcelofabianov@gmail.com>
2023-11-02 16:17:00 -0300

    chore: iniciando a ideia sobre infra e casos de uso

--- src/Infra/Database/Connection.php
+++ src/Infra/Database/Connection.php
@@ -1,10 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace SavingsMate\Infra\Database;
 
 use SavingsMate\Interfaces\Infra\Database\IConnection;
 
 final readonly class Connection implements IConnection
 {
-
 }
